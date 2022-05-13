23d589f6b0fa9170efe74dbf4a1f81384ef82a52
MateoFlorez <89142751+MateoFlorez@users.noreply.github.com>
2022-05-13 13:30:26 -0500

    Creo clase Cliente

--- Classes/Cliente.py
+++ Classes/Cliente.py
@@ -6,3 +6,40 @@ class Cliente:
         self.__cedula = None
         self.__ciudad = None
 
+    # GETTERS
+    @property
+    def nombre(self):
+        return(self.__nombre)
+
+    @property
+    def apellido(self):
+        return(self.__apellido)
+
+    @property
+    def cedula(self):
+        return(self.__cedula)
+
+    @property
+    def ciudad(self):
+        return(self.__ciudad)
+
+    # SETTERS
+    @nombre.setter
+    def nombre(self,nombre):
+        print('Digita el nombre: ')
+        self.__nombre = input(nombre)
+
+    @apellido.setter
+    def apellido(self,apellido):
+        print('Digita el apellido: ')
+        self.__apellido = input(apellido)
+
+    @cedula.setter
+    def cedula(self,cedula):
+        print('Digita la cedula: ')
+        self.__cedula = int(input(cedula))
+
+    @ciudad.setter
+    def ciudad(self,ciudad):
+        print('Digita la ciudad: ')
+        self.__ciudad = input(ciudad)
