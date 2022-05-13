3e512cf94223fb663520a2ccc6f600daf30a8133
MateoFlorez <89142751+MateoFlorez@users.noreply.github.com>
2022-05-13 13:20:17 -0500

    inicio ejercicio banco

---
+++ Classes/Cliente.py
@@ -0,0 +1,8 @@
+class Cliente:
+
+    def __init__(self):
+        self.__nombre = None
+        self.__apellido = None
+        self.__cedula = None
+        self.__ciudad = None
+
---
+++ Classes/Cuenta.py
@@ -0,0 +1,4 @@
+class Cuenta:
+
+    def __init__(self):
+        self.
